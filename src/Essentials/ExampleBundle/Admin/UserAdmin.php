85349fbcabc3e162918d273110dd5f0aa87dc171
Jonatan Frank <jonafrank@gmail.com>
2013-04-24 23:26:25 -0300

    Add UserAdmin Class

---
+++ src/Essentials/ExampleBundle/Admin/UserAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Essentials\ExampleBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class UserAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $form) 
+    {
+        $form->add('firstName', null, array('label' => 'First Name'))
+            ->add('lastName', null, array('label' => 'Last Name'))
+            ->add('dni', null, array('label' => 'DNI'))
+            ->add('birthDate', null, array('label' => 'Birth Date'));
+            
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $filter) 
+    {
+        $filter->add('firstName')
+                ->add('lastName')
+                ->add('dni');
+    }
+    
+    protected function configureListFields(ListMapper $list) 
+    {
+        $list->addIdentifier('dni')
+                ->add('firstName')
+                ->add('lastName')
+                ->add('birthDate');
+    }
+}
